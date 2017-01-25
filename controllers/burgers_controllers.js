c151e89fa8c6b6f0894aba5f144727833281a08e
kjige <chrisleongsc2@gmail.com>
2017-01-24 22:34:50 -0600

    test

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,30 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burgers.js) to use its database functions.
+var burgers = require("../models/burgers.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burgers.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function (req, res) {
+    burgers.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function () {
+        res.redirect("/");
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
